6b769e3f37b6469e597f9130c350bb98d3395f62
baejed <tristansubong@gmail.com>
2025-05-25 23:36:17 +0800

    feat: add stopwatch

--- lib/assets/styles.dart
+++ lib/assets/styles.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class Styles {
 
   static final txtHeading = TextStyle(color: Colors.white, fontWeight: FontWeight.w600);
-  static final txtTime = TextStyle(color: Colors. white, fontWeight: FontWeight.w700, fontSize: 48);
+  static final txtTimeMs = TextStyle(color: Colors. white, fontWeight: FontWeight.w700, fontSize: 52);
+  static final txtTime = TextStyle(color: Colors. white, fontWeight: FontWeight.w700, fontSize: 72);
   static final txtStats = TextStyle(color: Colors.white, fontWeight: FontWeight.w400);
 
 }
--- lib/pages/timer_page.dart
+++ lib/pages/timer_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:gun_draw_trainer/assets/styles.dart';
+
 
 class TimerPage extends StatefulWidget {
 
@@ -11,38 +14,84 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
+
+  Stopwatch stopwatch = Stopwatch();
+  double time = 0;
+  Duration duration = Duration();
+  Timer? timer;
+  int seconds = 0;
+  int ms = 0;
+  bool starting = false;
+
+  void start() {
+    stopwatch.start();
+    setState(() {
+      starting = true;
+    });
+    timer = Timer.periodic(Duration(milliseconds: 10), (Timer t) {
+      setState(() {
+        duration = stopwatch.elapsed;
+        seconds = stopwatch.elapsed.inSeconds;
+        ms = (stopwatch.elapsed.inMilliseconds%1000/10).toInt();
+      });
+    });
+  }
+
+  void stop() {
+    stopwatch.stop();
+    stopwatch.reset();
+    timer!.cancel();
+
+    setState(() {
+      starting = false;
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
+
     return (
-      Stack(
-        children: [
-          Center(
-            child: Text("2:24.13", style: Styles.txtTime,)
-          ),
-          Positioned(
-            bottom: 10,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(children: [
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                ],),
-                Column(children: [
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                  Text("Statistic: 123", style: Styles.txtStats,),
-                ],),
-              ],
+      GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTapDown: (details) => print("holding"),
+        onTapUp: (details) => starting ? stop() : start(),
+        child: Stack(
+          children: [
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text("$seconds.", style: Styles.txtTime),
+                  Text("${ms < 10 ? "0$ms" : ms}", style: Styles.txtTimeMs),
+                ],
+              )
+            ),
+            Positioned(
+              bottom: 10,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(children: [
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                  ],),
+                  Column(children: [
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                    Text("Statistic: 123", style: Styles.txtStats,),
+                  ],),
+                ],
+              )
             )
-          )
-        ],
+          ],
+        ),
       )
     );
   }
