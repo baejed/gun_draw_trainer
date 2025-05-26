b4280d012ad9e7329b31c9817f2ee1b6a02b78ba
baejed <tristansubong@gmail.com>
2025-05-26 14:20:43 +0800

    style: change color to a better shade of green

--- lib/res/styles.dart
+++ lib/res/styles.dart
@@ -6,11 +6,11 @@ class Styles {
 
   static final txtTimeMs = TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 52);
   static final txtTimeMsDarker = TextStyle(color: Colors.grey[400], fontWeight: FontWeight.w700, fontSize: 52);
-  static final txtTimeMsGreen = TextStyle(color: Colors.green, fontWeight: FontWeight.w700, fontSize: 52);
+  static final txtTimeMsGreen = TextStyle(color: Colors.green[400], fontWeight: FontWeight.w700, fontSize: 52);
 
   static final txtTime = TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 72);
   static final txtTimeDarker = TextStyle(color: Colors.grey[400], fontWeight: FontWeight.w700, fontSize: 72);
-  static final txtTimeGreen = TextStyle(color: Colors.green, fontWeight: FontWeight.w700, fontSize: 72);
+  static final txtTimeGreen = TextStyle(color: Colors.green[400], fontWeight: FontWeight.w700, fontSize: 72);
 
   static final txtStats = TextStyle(color: Colors.white, fontWeight: FontWeight.w400);
 
